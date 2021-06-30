fead3810d79b600ac6f9a12a6d484424fdb7704d
Brandon Knox <bknox.dev@gmail.com>
2021-06-30 11:51:31 -0700

    example of manual positioning with text background color.  size of background color based on parent/child relationships swift uses when rendering.

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -7,41 +7,11 @@
 
 import SwiftUI
 
-extension VerticalAlignment {
-    enum MidAccountAndName: AlignmentID {
-        static func defaultValue(in d: ViewDimensions) -> CGFloat {
-            d[.top]
-        }
-    }
-    
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
-}
-
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@twostraws")
-                    .alignmentGuide(.midAccountAndName) { d in
-                        d[VerticalAlignment.center]
-                    }
-                Image("background")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-            }
-            
-            VStack {
-                Text("More text")
-                Text("More text")
-                Text("More text")
-                Text("Full name:")
-                Text("PAUL HUDSON")
-                    .alignmentGuide(.midAccountAndName) { d in
-                        d[VerticalAlignment.center]
-                    }
-                    .font(.largeTitle)
-            }
-        }
+        Text("Hello, world!")
+            .background(Color.red)
+            .position(x: 100, y: 100)
     }
 }
 
