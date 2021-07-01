17be8994e831a7c77861afd9a7b0188fcec7beea
Brandon Knox <bknox.dev@gmail.com>
2021-06-30 17:40:56 -0700

    Created a scrollview with GeometryReader, alternating colors in each text view inside using colors array.

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -7,41 +7,23 @@
 
 import SwiftUI
 
-struct OuterView: View {
-    var body: some View {
-        VStack {
-            Text("Top")
-            InnerView()
-                .background(Color.green)
-            Text("Bottom")
-        }
-    }
-}
-
-struct InnerView: View {
-    var body: some View {
-        HStack {
-            Text("Left")
-            GeometryReader { geo in
-                Text("Center")
-                    .background(Color.blue)
-                    .onTapGesture {
-                        print("Global center: \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
-                        print("Custom center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
-                        print("Local center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
-                    }
-            }
-            .background(Color.orange)
-            Text("Right")
-        }
-    }
-}
-
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    
     var body: some View {
-        OuterView()
-            .background(Color.red)
-            .coordinateSpace(name: "Custom")
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(width: fullView.size.width)
+                            .background(self.colors[index % 7])
+                    }
+                    .frame(height: 40)
+                }
+            }
+        }
     }
 }
 
